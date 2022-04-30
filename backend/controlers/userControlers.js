3692deafe2b2072cff00d77b9dafaf1b664fe8b2
wilowdev <patryk.wierzba.pw@gmail.com>
2022-04-30 13:14:44 +0200

    errors in register

--- backend/controlers/userControlers.js
+++ backend/controlers/userControlers.js
@@ -1,6 +1,20 @@
+//@Desc     Register new user
+//@route    /api/users
+//@access   Public
 const registerUser = (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
+
   res.send('Register Route');
 };
+
+//@Desc    login user
+//@route   /api/users/login
+//@access  Public
 const loginUser = (req, res) => {
   res.send('Login Route');
 };
