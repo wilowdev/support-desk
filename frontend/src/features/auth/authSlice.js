c5628c18ad750d42c1ffc9961c696ce530706fe6
wilowdev <patryk.wierzba.pw@gmail.com>
2022-05-01 19:50:38 +0200

    hook register & login to redux

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -8,6 +8,17 @@ const initialState = {
   message: '',
 };
 
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    console.log(user);
+  }
+);
+
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+  console.log(user);
+});
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState: initialState,
